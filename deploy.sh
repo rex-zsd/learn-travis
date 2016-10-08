90b98500d1d052bc0cdd11bf417b4b85c9c4959b
rex <rexkaneki@gmail.com>
2016-10-08 18:01:10 +0800

    1

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 echo "hello world!"
-scp -i deploy_rsa -o StrictHostKeyChecking=no -r dist rex@45.32.48.52:/home/rex
+scp -i deploy_rsa -o StrictHostKeyChecking=no package.json rex@45.32.48.52:/home/rex
